bf99a20ff61cf2d0052707b74666077ee59dd70f
karthickuid <karthickuid@m2p.in>
2021-01-19 12:56:26 +0530

    first commit

---
+++ js/chartjs.js
@@ -0,0 +1,330 @@
+
+$(document).ready(function () {
+
+    setTimeout(function () {
+
+        new Chartist.Pie('.total-chart', {
+            series: [20, 10],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true
+        });
+
+        new Chartist.Pie('.active-chart', {
+            series: [47, 53],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>47%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.inactive-chart', {
+            series: [50, 50],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>50%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.kyc-chart', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.region-chart', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.total-cards', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true
+        });
+
+        new Chartist.Pie('.alloc-cards', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.unalloc-cards', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.active-cards', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.inactive-cards', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.replace-card', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.perm-block', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.temp-block', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.total-trans', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true
+        });
+
+        new Chartist.Pie('.succ-trans', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.fail-trans', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.mer-trans', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.marrej-trans', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+        new Chartist.Pie('.init-trans', {
+            series: [60, 40],
+        }, {
+            donut: true,
+            donutWidth: 30,
+            height: 130 + 'px',
+            donutSolid: true,
+            startAngle: 0,
+            showLabel: true,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<h3>60%</h3>'
+                    }]
+                })
+            ],
+        });
+
+    }, 200)
+
+})
+
---
+++ js/main.js
@@ -0,0 +1,342 @@
+// load content profile
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+  //profile pic dropdown
+  $(".profile-username").click(function () {
+    $(".profile-dlt-sec").toggle();
+  });
+
+  // logo nav
+  $(".logo").click(function () {
+    window.location.href = "index.html";
+  });
+
+  // logo index
+  $(".logo-act").click(function () {
+    window.location.href = "index.html";
+  });
+
+
+  setTimeout(function () {
+
+    var dnuts;
+    $(".dounut-chrt-sec").each(function () {
+      dnuts = $(this).find(".dnt-chrt");
+
+      if ($(window).width() < 1600) {
+        toShow = 5;
+        dnuts.hide();
+        dnuts.slice(0, toShow).show();
+      } else {
+        toShow = 6;
+        dnuts.hide();
+        dnuts.slice(0, toShow).show();
+      }
+
+    })
+
+    $(".dounut-chrt-sec").addClass("dhide");
+
+    $(".more-charts").click(function () {
+
+      dnuts = $(this).parent().find(".dounut-chrt-sec").find(".dnt-chrt");
+
+      if ($(window).width() < 1600) {
+        toShow = 5;
+        dnuts.hide();
+        dnuts.slice(0, toShow).show();
+      } else {
+        toShow = 6;
+        dnuts.hide();
+        dnuts.slice(0, toShow).show();
+      }
+
+      if ($(this).parent().find(".dounut-chrt-sec").hasClass("dhide")) {
+        dnuts.show();
+        $(this).parent().find(".dounut-chrt-sec").removeClass("dhide");
+      } else {
+        dnuts.hide();
+        dnuts.slice(0, toShow).show();
+        $(this).parent().find(".dounut-chrt-sec").addClass("dhide");
+      }
+
+    });
+  }, 400);
+
+
+
+
+  // mobile toggle nav
+  $(".mobile-togle-nav").click(function () {
+    $(".mob-nav-section").show();
+  });
+  $(".nav-tog-close").click(function () {
+    $(".mob-nav-section").hide();
+  });
+
+  $('.drop-navs').on('click', function () {
+    $("#nav-drop-items").parent().toggleClass("active");
+  });
+
+  $(".amt-added").click(function () {
+    $('.toaster').stop().fadeIn(400).delay(2000).fadeOut(500);
+  });
+
+  $(".profile-pic").hover(function () {
+    $(this).toggleClass("upload-pic");
+  });
+
+
+
+  // $(document).on("click", "#nav-drop-items", function (e) {
+
+  //   $("#nav-drop-items").parent().toggleClass("drop-list-nav");
+  // })
+  // sidenav - dropdown
+  // $(document).on("click", "#nav-drop-items", function () {
+  //   $(this).addClass("drop-list-nav");
+  //   // if ($(this).hasClass("collapsed")) {
+  //   //   $(this).removeClass("drop-list-nav");
+  //   // }
+  // }, function () {
+  //   $(this).removeClass("drop-list-nav");
+  // });
+
+  // $(".toggle-nav").click(function () {
+  //   $(".navbar-nav .nav-txt, .poweredby .nav-txt").toggle();
+  //   $(".logo-act").toggle();
+  //   $(".logo-tog").toggle();
+  //   $(".navpanel").toggleClass("nav-toggle");
+  //   $(".contenet-sec").toggleClass("contenet-sec-toggle");
+  //   $(".dropdown-item").parents(".nav-item").removeClass("dropclass");
+  // });
+
+  // $(document)
+  //   .on("mouseenter", ".nav-toggle", function () {
+  //     $(".nav-toggle").addClass("nav-toggle-hover");
+  //     $(".navbar-nav .nav-txt, .poweredby .nav-txt").show();
+  //     $(".logo-act").show();
+  //     $(".logo-tog").hide();
+  //     if ($(".nav-item").hasClass("dropclass")) {
+  //       $(".dropdown-item").parents(".nav-item").addClass("dropclass");
+  //     }
+  //   })
+  //   .on("mouseleave", ".nav-toggle", function () {
+  //     $(".nav-toggle").removeClass("nav-toggle-hover");
+  //     $(".navbar-nav .nav-txt, .poweredby .nav-txt").hide();
+  //     $(".logo-act").hide();
+  //     $(".logo-tog").show();
+  //     $(".dropdown-item").parents(".nav-item").removeClass("dropclass");
+  //   });
+
+  // kabeb menu
+  $(".kabeb").on("click", function () {
+    $(this).parent().find(".kabeb-list").toggle();
+  });
+  // outside click to hide kabeb list
+  $(document).mouseup(function (e) {
+    var container = $(".kabeb-list, .profile-dlt-sec ");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      container.hide();
+    }
+  });
+
+  // for popup
+  $(".blk-tag").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-block").show();
+  });
+  // edit transaction popup
+
+  $(".edit-trans-queue").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-edit-transq").show();
+  })
+
+  $('.add-blklist').click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-add-blocklist").show();
+  })
+
+  $(".rep-tag").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-replace").show();
+  });
+  $(".mapr-tag").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-mapper").show();
+  })
+  // popup-cancel
+  $(".cncl-butn-pop").click(function () {
+    $(this).parents(".pop-cont").hide()
+    $(".overlay").hide();
+  })
+  // vehicle mapper popup
+  $(".veh-tag").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-map-veh").show()
+  })
+  // change password
+  $(".change-password").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-change-password").show();
+  });
+  // cancel password
+  $(".cncl-password").click(function () {
+    $(".change-password-show").hide();
+    $(".change-password").show();
+
+  });
+  // custom date seclection
+  $(document).on("click", ".calc", function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-custom-date").show();
+  })
+  // fetch-data - delete after complete UI
+  $(".fetch-date").click(function () {
+    $(".custom-date-show").show();
+    $(".overlay").hide();
+    $(".pop-cont").hide();
+    $(".popup-custom-date").hide();
+  });
+  // edit custom date
+  $(".custom-date-edit").click(function () {
+    $(".overlay").show();
+    $(".pop-cont").hide();
+    $(".popup-custom-date").show();
+  });
+
+
+  // empty transaction view
+  if ($(".table-transaction-history tr").length < 1) {
+    $(".table-transaction-history").hide();
+    $(".empty-trans-section").css("display", "flex");
+    $(".trans-tbl-cnt").hide();
+  } else {
+    $(".table-transaction-history").show();
+    $(".empty-trans-section").hide();
+    $(".trans-tbl-cnt").show();
+  }
+
+
+  // drop-down menu
+
+  // $(".drop-navs").click(function () {
+  //   $(this).toggleClass("drop-arrow")
+  // })
+
+  // forget password
+  $(".forget-pass").click(function () {
+    $(".log-login").hide();
+    $(".login-form").hide();
+    $(".log-forget").show();
+    $(".forget-form").show();
+  });
+  $(".btn-otp-start").click(function () {
+    $(".otp-bx,.btn-otp-verify").show();
+    $(this).hide();
+  });
+  $(".btn-otp-verify").click(function () {
+    $(".forget-form").hide();
+    $(".login-form").hide();
+    $(".change-password-form").show();
+  });
+
+  $(".return-login").click(function () {
+    $(".log-login").show();
+    $(".login-form").show();
+    $(".log-forget").hide();
+    $(".forget-form").hide();
+    $(".change-password-form").hide();
+  });
+  $(".btn-signin").click(function () {
+    window.location.href = "index.html";
+  })
+
+
+});
+
+$(document).ready(function () {
+  $(".bootstrap-select button").css("outline", "none !important");
+
+  // upload sec
+  $(".file-upload").each(function () {
+    $(this).change(function () {
+      filename = this.files[0].name;
+      $(this).parent().find(".file-up").text(filename);
+    });
+  });
+
+  // custom selct
+
+  // create new variable for each menu
+  // var dd2 = new DropDown($('#other-gases'));
+  // $(document).click(function () {
+  //   // close menu on document click
+  //   $(".wrap-drop").removeClass("active");
+  // });
+
+  $(".drop li").click(function () {
+    $(".drop li").removeClass("drop-active");
+    $(this).addClass("drop-active");
+  });
+});
+
+// custom dropdown - for tag class - add vehicle section
+function DropDown(el) {
+  this.dd = el;
+  this.placeholder = this.dd.children("span");
+  this.opts = this.dd.find("ul.drop li");
+  this.val = "";
+  this.index = -1;
+  this.initEvents();
+}
+
+DropDown.prototype = {
+  initEvents: function () {
+    var obj = this;
+    obj.dd.on("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(this).toggleClass("active");
+    });
+    obj.opts
+      .on("click", function () {
+        var opt = $(this);
+        obj.val = opt.text();
+        obj.index = opt.index();
+        obj.placeholder.text(obj.val);
+        opt.siblings().removeClass("selected");
+        opt.filter(':contains("' + obj.val + '")').addClass("selected");
+      })
+      .change();
+  },
+  getValue: function () {
+    return this.val;
+  },
+  getIndex: function () {
+    return this.index;
+  },
+};
+
+// active nav
+setTimeout(function () {
+  $(".nav-item").each(function () {
+    if ($(this).hasClass("active")) {
+      $(this).find(".add-detail-btn").show();
+    }
+  });
+}, 200);
+// back to history
+$(".tlt-bck, .cncl-butn").click(function () {
+  window.history.back();
+});
+
